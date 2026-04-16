1d963cbe1bfcd2d7c14f14649dfb9d76767dd67c
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-04-16 17:05:12 +0800

    feat(agent): implement memorize method with tape support

--- src/tape_mem/agents/tape.py
+++ src/tape_mem/agents/tape.py
@@ -1,22 +1,44 @@
+from tape_mem.dataset.templates import Template
 from tape_mem.types import Agent
 from tape_mem.types.agent import AgentResponse
 from tape_mem.types.provider import ProviderConfig
-from republic import LLM
+from republic import LLM, TapeEntry
 
 
 class TapeAgent(Agent):
-    def __init__(self, provider: ProviderConfig):
-        self.llm = LLM(
+    def __init__(self, provider: ProviderConfig, template: Template):
+        self._template = template
+        self._llm = LLM(
             model=f"openai:{provider.model}",
             api_key=provider.api_key,
             api_base=provider.base_url,
         )
 
         # create a new tape and write on it
-        self.llm.tape("main")
+        self._active_tape = self._llm.tape("main")
+        self._counter: int = 0
 
     def memorize(self, chunk: str) -> None:
-        raise NotImplementedError
+        self._active_tape.handoff(f"chunk_{self._counter}", state={"memorize": True})
+        self._counter += 1
+
+        # write the chunk on tape
+        self._active_tape.append(
+            TapeEntry.message(
+                # todo: using template (blocking by template refactoring)
+                {"role": "user", "content": chunk}
+            )
+        )
+        # pretend that agent responses
+        self._active_tape.append(
+            TapeEntry.message(
+                # todo: using template (blocking by template refactoring)
+                {
+                    "role": "user",
+                    "content": "I have learned the documents and I will answer the question you ask.",
+                }
+            )
+        )
 
     def forget(self, chunk: str) -> None:
         raise NotImplementedError
